f624cd148527b00bd8664fa96574ff32124e1ba6
Mateusz Gadzina <gadzinam28@gmail.com>
2025-12-03 09:31:27 +0100

    Added Theme Button
    Just added button that allows the user to switch theme between light and dark

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import { click } from '@testing-library/user-event/dist/click';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 interface HeaderProps {
     title: string;
     subtitle?: string;
@@ -12,6 +12,12 @@ const Header: React.FC<HeaderProps> = ({ title, subtitle = "Default subtitle", v
         setClickCount(clickCount + 1);
         alert(`Header clicked ${clickCount + 1} times`);
     }
+    type theme = 'light' | 'dark';
+    const [theme, setTheme] = useState<theme>('light');
+    const toggleTheme = () => {
+        setTheme(theme === 'light' ? 'dark' : 'light');
+    }
+    
     return (
     <header style={{
         backgroundColor: '#f0f0f0' ,
@@ -27,6 +33,16 @@ const Header: React.FC<HeaderProps> = ({ title, subtitle = "Default subtitle", v
         <button onClick={handleClick} style={{
             marginTop: '10px',}}>click me</button>
         {clickCount > 0 && (<p>Header clicked {clickCount} times</p>)}
+        <button onClick={toggleTheme}>Change theme</button>
+        {theme === 'light' ? (
+            <div style={{ marginTop: '10px', padding: '10px', backgroundColor: '#fff', color: '#000' }}>
+                Light Theme Active
+            </div>
+        ) : (
+            <div style={{ marginTop: '10px', padding: '10px', backgroundColor: '#333', color: '#fff' }}>
+                Dark Theme Active
+            </div>
+        )}
     </header>
     );
 }
